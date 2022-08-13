eba6d0410d05e87b1a47d9a911211eb8739700f6
Mooner510 <lmck510@gmail.com>
2022-08-13 17:29:56 +0900

    Added When first joined server, run tutorial.

--- src/main/java/org/mooner/litejoin/LiteJoin.java
+++ src/main/java/org/mooner/litejoin/LiteJoin.java
@@ -4,15 +4,19 @@ import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.mooner.moonerbungeeapi.api.BungeeAPI;
+import org.mooner.moonerbungeeapi.api.ServerType;
 
 public final class LiteJoin extends JavaPlugin implements Listener {
     public static LiteJoin plugin;
+    private static int port;
 
     public static String chat(String msg){
         return ChatColor.translateAlternateColorCodes('&', msg);
@@ -21,6 +25,7 @@ public final class LiteJoin extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         plugin = this;
+        port = Bukkit.getServer().getPort();
         this.getLogger().info("Plugin Enabled!");
         Bukkit.getPluginManager().registerEvents(this, this);
         // Plugin startup logic
@@ -39,6 +44,13 @@ public final class LiteJoin extends JavaPlugin implements Listener {
             p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(chat("&f&l[&a&l✔&f&l] &f" + e.getPlayer().getName())));
         }
         Bukkit.getConsoleSender().sendMessage(chat("&e" + e.getPlayer().getName() + " joined the server"));
+        if(BungeeAPI.getServerType(port) == ServerType.SPAWN_SERVER) {
+            if(e.getPlayer().hasPlayedBefore()) {
+                e.getPlayer().teleport(new Location(Bukkit.getWorld("world"), 0.5, 65, 0.5));
+            } else {
+                e.getPlayer().teleport(new Location(Bukkit.getWorld("world"), 0.5, -55.5, 0.5));
+            }
+        }
     }
 
     @EventHandler
